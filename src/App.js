71635765159bcae1516b9a5ab44b89033bac1426
Andres Manrique <andres.feego@gmail.com>
2021-09-23 17:19:54 -0500

    cambios en app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          cambio de app.js
         </p>
         <a
           className="App-link"
